a95c992527b231ddb37346c0ed92c32355239452
noerdzc <61292187+noerdzc@users.noreply.github.com>
2021-03-05 20:39:59 -0600

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDXhgO40V40C8GMDfQiRnZSBn4PpegtzmexKuQJrG8+DWBM9lICpjSOglevMgNCCMW2RxclyHw0qca9131sy1lO93xRdsH1gQxv8pMeQFVe9gaK+vxa0U4G3zC7cHCq57Z7ZCh5wT2O1h+GjApUIOrL8fZkW1jRS36SXM7RQiZ4usOjlFzmKU4WAE1ngU3N6J98D+YQGRSQHKsNIebmFaGkhN51csPxEMJOvxAE4GTtF2Kr9zEPPogdxUZCm20dbXqKhLAO+UBHA0zJ3qkN+LvGqJvStaTeQa4gHkftH+vJPa6lIUnQxDycH81MmJW7WRgt6MjAb2vFYI1ma9+7Qeej',
 	}  
 }
